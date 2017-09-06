770a47f580ca0168740895f9e50013f953b06bdd
Joseph Gordy <jgordy2424@gmail.com>
2017-09-06 18:22:41 -0400

    Signup functionality working. Tackling login next

--- routes/base.js
+++ routes/base.js
@@ -1,19 +1,72 @@
 const express = require("express");
+const models = require("../models/index");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
+const passport = require('passport');
+
+const isAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    req.flash('error', 'You have to be logged in to access the page.')
+    res.redirect('/')
+  }
 
 router.get("/", function(req, res) {
+
+  res.render("signup", {
+      messages: res.locals.getMessages()
+  });
+});
+
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/feed',
+    failureRedirect: '/',
+    failureFlash: true
+}));
+
+router.get("/signup", isAuthenticated, function(req, res) {
   res.render("signup");
 });
 
+router.post("/signup", function(req, res) {
+  let username = req.body.username
+  let password = req.body.password
+  let name     = req.body.name
+  let confirmPassword = req.body.confirmPassword
 
+  if (!username || !password) {
+    req.flash('error', "Please, fill in all the fields.")
+    res.redirect('signup')
+  }
 
+  let salt = bcrypt.genSaltSync(10)
+  let passwordHash = bcrypt.hashSync(password, salt)
 
+  let newUser = {
+    name: name,
+    username: username,
+    salt: salt,
+    password: passwordHash
+  }
 
+  if (password === confirmPassword) {
 
+    models.User.create(newUser)
+    .then(function() {
 
+      res.redirect("/");
+    })
+    .catch(function(error) {
+      req.flash('error', "Please, choose a different username.")
+      res.redirect('/signup')
+    });
 
-
-
+  } else {
+    req.flash("error", "Password does not match")
+    res.redirect("/signup")
+  }
+});
 
 module.exports = router;
