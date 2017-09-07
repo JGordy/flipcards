b92560cad0b9e64c27b27286a0488b21fb50c213
Joseph Gordy <jgordy2424@gmail.com>
2017-09-07 15:24:58 -0400

    remove and edit flipcard api endpoints complete

--- routes/api.js
+++ routes/api.js
@@ -7,12 +7,14 @@ const BasicStrategy   = require('passport-http').BasicStrategy;
 
 // √ root end point
 router.get("/",  passport.authenticate('basic', {session: false}), function(req, res) {
+
   res.status(200).send("This is my api documentation. Yeah it's not much, but she's got it where it counts kid");
 });
 
 
 // √ get the collection of created decks
 router.get("/decks", passport.authenticate('basic', {session: false}), function(req, res) {
+
   models.Deck.findAll({})
   .then(function(data) {
     if (data) {
@@ -47,6 +49,7 @@ router.post("/new_deck", passport.authenticate('basic', {session: false}), funct
 
 // √ create flipcards within a deck
 router.post("/new_card/:id", passport.authenticate('basic', {session: false}), function(req, res) {
+
   models.Card.create({
     deckId: req.params.id,
     front: req.body.front,
@@ -61,15 +64,47 @@ router.post("/new_card/:id", passport.authenticate('basic', {session: false}), f
 });
 
 
-// edit a single flipcard within a deck
+// √ edit a single flipcard within a deck
 router.put("/edit_flipcard/:deckId/:id", passport.authenticate('basic', {session: false}), function(req, res) {
-  res.status(200);
+
+  models.Card.update({
+    front: req.body.front,
+    back: req.body.back
+  }, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(function(data) {
+    res.status(201).send(data)
+  })
+  .catch(function(err) {
+    res.status(500).send(err)
+  })
 });
 
 
-// removing a single flipcard from a deck
+// √ removing a single flipcard from a deck
 router.delete("/remove/:deckId/:id", passport.authenticate('basic', {session: false}), function(req, res) {
-  res.status(200);
+
+  models.Card.findOne({
+    where: {id: req.params.id}
+  })
+  .then(function(data) {
+    models.Card.destroy({
+      where: {id: req.params.id}
+    })
+    .then(function(data) {
+      res.sendStatus(200).send(data);
+    })
+    .catch(function(err) {
+      res.status(500).send(err)
+    })
+  })
+  .catch(function(err) {
+    res.status(500).send(err)
+  })
+
 });
 
 module.exports = router;
